8a5d367e27f8cd356828d76f49b635a9aa4caebb
andreas <andypatsalos@gmail.com>
2024-05-14 12:34:10 +0300

    Completed the About Me subpage.

---
+++ Javascript/clock.js
@@ -0,0 +1,19 @@
+﻿"use strict"
+
+runClock();
+setInterval(runClock, 1000);
+function runClock() {
+   let thisDay = new Date();
+   let thisDate = thisDay.toLocaleDateString();
+   let thisDayNum = thisDay.getDay();
+   let thisWeekday = getWeekday(thisDayNum);
+   let thisTime = thisDay.toLocaleTimeString();
+   document.getElementById("date").textContent = thisDate;
+   document.getElementById("wday").textContent = thisWeekday;
+   document.getElementById("time").textContent = thisTime;
+}
+
+function getWeekday(dayNum) {
+   var wDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   return wDays[dayNum];
+}
